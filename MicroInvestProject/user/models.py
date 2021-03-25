63aac50ff1865d7f1182664f40990f78f3657d9c
Tahmid Anam <tahmidshovon30@gmail.com>
2021-03-25 20:15:16 +0600

    User Table Created

--- MicroInvestProject/user/models.py
+++ MicroInvestProject/user/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
 # Create your models here.
+
+class User(models.Model):
+    name = models.TextField()
+    nid = models.IntegerField(unique=True)
+    dob = models.DateField()
+    cell = models.IntegerField(max_length=11)
+    email = models.EmailField()
+    password = models.TextField()
+    isActive = models.BooleanField(default=False)
+    isInvestor = models.BooleanField(default=False)
+    isEntrepreneur = models.BooleanField(default=False)
+    isAnalyst = models.BooleanField(default=False)
+    totalInvested = models.FloatField(default=0.00)
+    totalWithdrawn = models.FloatField(default=0.00)
